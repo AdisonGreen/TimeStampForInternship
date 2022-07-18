09ec423f1b3d97d8b46016df4564d09470057664
Adison Green <adison@adisons-mbp.lan>
2022-07-18 16:37:48 -0600

    First Commit

---
+++ TimeStampForInternship/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeStampForInternship
+//
+//  Created by Adison Green on 7/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
