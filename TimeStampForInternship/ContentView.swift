a6b122428f33e27fe688e1a2dbdf24c34c986a77
Adison Green <adison@adisons-mbp.lan>
2022-07-19 11:54:52 -0600

    Add 10 buttons to the screen
    When buttons pressed it shows the time it was pressed at

--- TimeStampForInternship/ContentView.swift
+++ TimeStampForInternship/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var buttonWasPressed = false
+    @State private var TimeOfButtonPress = Text("")
+    let howManyButtons = 10
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ForEach(1...howManyButtons, id: \.self) { number in
+                HStack {
+                    Button(action: buttonPressed) {
+                        ZStack {
+                            Text("\(number)")
+                                .foregroundColor(.black)
+                                .zIndex(1)
+                            Image(systemName: "circle.fill")
+                        }
+                    }
+                    .alert(TimeOfButtonPress, isPresented: $buttonWasPressed) { }
+                }
+            }
+        }
+        
+    }
+    
+    func buttonPressed() {
+        let timeOfButtonPressed = Text(Date.now, format: .dateTime.hour().minute().second())
+        TimeOfButtonPress = timeOfButtonPressed
+        buttonWasPressed = true
     }
 }
 
