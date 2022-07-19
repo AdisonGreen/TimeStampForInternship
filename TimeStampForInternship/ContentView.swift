c96f5f05dcf940d8ebfee0e49067c080589fa835
Adison Green <adison@adisons-mbp.lan>
2022-07-19 14:13:55 -0600

    Aranged buttons to look better

--- TimeStampForInternship/ContentView.swift
+++ TimeStampForInternship/ContentView.swift
@@ -9,31 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var buttonWasPressed = false
-    @State private var TimeOfButtonPress = Text("")
+    @State private var timeOfButtonPress = Text("")
     let howManyButtons = 10
     
     var body: some View {
-        VStack {
-            ForEach(1...howManyButtons, id: \.self) { number in
-                HStack {
-                    Button(action: buttonPressed) {
-                        ZStack {
-                            Text("\(number)")
-                                .foregroundColor(.black)
-                                .zIndex(1)
-                            Image(systemName: "circle.fill")
+        HStack {
+            Spacer()
+            VStack {
+                Spacer()
+                ForEach(0...howManyButtons, id: \.self) { number in
+                    let numberOfButtons = number % 2
+                    if numberOfButtons != 0 {
+                        Button(action: buttonPressed) {
+                            ZStack {
+                                Text("\(number)")
+                                    .foregroundColor(.black)
+                                    .zIndex(1)
+                                Image(systemName: "square.fill")
+                            }
                         }
+                        .scaleEffect(3)
+                        .alert(timeOfButtonPress, isPresented: $buttonWasPressed) { }
+                        Spacer()
                     }
-                    .alert(TimeOfButtonPress, isPresented: $buttonWasPressed) { }
                 }
             }
+            Spacer()
+            VStack {
+                Spacer()
+                ForEach(1...howManyButtons, id: \.self) { number in
+                    let numberOfButtons = number % 2
+                    if numberOfButtons == 0 {
+                        Button(action: buttonPressed) {
+                            ZStack {
+                                Text("\(number)")
+                                    .foregroundColor(.black)
+                                    .zIndex(1)
+                                Image(systemName: "square.fill")
+                            }
+                        }
+                        .scaleEffect(3)
+                        .alert(timeOfButtonPress, isPresented: $buttonWasPressed) { }
+                        Spacer()
+                    }
+                }
+            }
+            Spacer()
         }
-        
     }
     
     func buttonPressed() {
         let timeOfButtonPressed = Text(Date.now, format: .dateTime.hour().minute().second())
-        TimeOfButtonPress = timeOfButtonPressed
+        timeOfButtonPress = timeOfButtonPressed
         buttonWasPressed = true
     }
 }
