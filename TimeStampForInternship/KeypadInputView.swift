3773c38b37f9e780c762fd9e5a98416e382f61f0
Adison Green <adison@adisons-mbp.lan>
2022-07-29 17:45:41 -0600

    Keypad saving to the data dictionary

--- TimeStampForInternship/KeypadInputView.swift
+++ TimeStampForInternship/KeypadInputView.swift
@@ -15,6 +15,8 @@ struct KeypadInputView: View {
     var body: some View {
         NavigationView {
             TextField("Press a key", text: $userInput)
+                .keyboardType(.numberPad)
+                
                 .onChange(of: userInput) { newValue in
                     inputInTextField = true
                     userInput = ""
@@ -42,43 +44,51 @@ struct KeypadInputView: View {
         switch input {
         case "0":
             print("Button 0 was pressed")
+            buttonPressed(buttonNumber: "0")
         case "1":
             print("Button 1 was pressed")
+            buttonPressed(buttonNumber: "1")
         case "2":
             print("Button 2 was pressed")
+            buttonPressed(buttonNumber: "2")
         case "3":
             print("Button 3 was pressed")
+            buttonPressed(buttonNumber: "3")
         case "4":
             print("Button 4 was pressed")
+            buttonPressed(buttonNumber: "4")
         case "5":
             print("Button 5 was pressed")
+            buttonPressed(buttonNumber: "5")
         case "6":
             print("Button 6 was pressed")
+            buttonPressed(buttonNumber: "6")
         case "7":
             print("Button 7 was pressed")
+            buttonPressed(buttonNumber: "7")
         case "8":
             print("Button 8 was pressed")
+            buttonPressed(buttonNumber: "8")
         case "9":
             print("Button 9 was pressed")
+            buttonPressed(buttonNumber: "9")
         case "":
-            print("Worthless print")
+            print("Case to stop default from being called")
         default:
             print("Not valid input")
         }
     }
     
-//    func buttonPressed(button: String) {
-//        
-//        if buttonPressedForFirstTime == true {
-//            buttonPressData.append([button: timeOfButtonPress])
-//            print(buttonPressData)
-//        } else {
-//            buttonPressData = [[button: timeOfButtonPress]]
-//            print(buttonPressData)
-//            buttonPressedForFirstTime = true
-//        }
-//        
-//    }
+    func buttonPressed(buttonNumber: String) {
+        let timeOfButtonPress = "\(Date.now.formatted(.dateTime.hour().minute().second()))"
+        
+        if buttonPressedForFirstTime == true {
+            buttonPressData.append([buttonNumber: timeOfButtonPress])
+        } else {
+            buttonPressData = [[buttonNumber: timeOfButtonPress]]
+            buttonPressedForFirstTime = true
+        }
+    }
 }
 
 struct KeypadInputView_Previews: PreviewProvider {
